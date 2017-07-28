784e435fd2a2a42612ec453e158a9d9955a19a29
Masqurin <Masqurin@icloud.com>
2017-07-28 10:45:40 +0800

    完成

--- HelloMyWidget/AppDelegate.m
+++ HelloMyWidget/AppDelegate.m
@@ -46,7 +46,7 @@
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
-
+//外部呼叫方法
 -(BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation{
     //"myWidget://12345"
     //url呼叫者帶來的東西
